add normalization, other fixes
1) Add normalization into dataset.py. 2) Add exception into dataset.py if labels broken. 3) Add standalone training script. 3) Add seed initializer for workers.

src/dataset.py
@@ -201,7 +201,7 @@ class JointDataset:
             max_index = -1
             for lp in label_paths:
                 lb = np.loadtxt(lp)
-                if lb.shape[0] < 1:
+                if lb.size < 1:
                     continue
                 if lb.ndim < 2:
                     img_max = lb[1]
@@ -223,7 +223,9 @@ class JointDataset:
         self.width = img_size[0]
         self.height = img_size[1]
         self.augment = augment
-        self.img_scale = config.img_scale
+        self.img_scale = 1. / config.img_scale
+        self.mean = np.array(config.img_mean).reshape((3, 1, 1))
+        self.std = np.array(config.img_std).reshape((3, 1, 1))
 
         print('=' * 40)
         print('dataset summary')
@@ -278,7 +280,7 @@ class JointDataset:
             labels[:, 4] = ratio * w * (labels0[:, 2] + labels0[:, 4] / 2) + padw
             labels[:, 5] = ratio * h * (labels0[:, 3] + labels0[:, 5] / 2) + padh
         else:
-            labels = np.array([])
+            labels = np.array([0, -1, 0, 0, 0, 0])
 
         # Augment image and labels
         if self.augment:
@@ -292,6 +294,7 @@ class JointDataset:
             labels[:, 3] /= height
             labels[:, 4] /= width
             labels[:, 5] /= height
+
         if self.augment:
             # random left-right flip
             lr_flip = True
@@ -302,7 +305,7 @@ class JointDataset:
 
         img = np.ascontiguousarray(img[:, :, ::-1])  # BGR to RGB
 
-        img = img.transpose(2, 0, 1) / self.img_scale
+        img = (self.img_scale * img.transpose((2, 0, 1)) - self.mean) / self.std
 
         return img, labels, img_path
 
@@ -321,6 +324,12 @@ class JointDataset:
         for i, _ in enumerate(labels):
             if labels[i, 1] > -1:
                 labels[i, 1] += self.tid_start_index[ds]
+
+        # If image without pedestrians detections
+        # We dont drop that image, but learn network to
+        # Make less true negatives predictions
+        if labels.size == 0:
+            labels = np.array([[0, -1, 0, 0, 0, 0]])
 
         # Calculate confidence mask, bbox delta and ids for every map size
         small, medium, big = build_thresholds(

train.py
@@ -17,8 +17,15 @@ from src.model import YOLOv3
 from src.utils import collate_fn
 from src.utils import load_darknet_weights
 
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+
+def worker_init_fn(worker_id):
+    """
+    Initialize worker seed.
+
+    Args:
+        worker_id: Id of the current cpu worker.
+    """
+    np.random.seed(np.random.get_state()[1][0] + worker_id)
 
 
 def init_train_model(cfg, nid):
@@ -88,6 +95,8 @@ def main():
         batch_size=config.batch_size,
         shuffle=True,
         drop_last=True,
+        num_workers=8,
+        worker_init_fn=worker_init_fn,
     )
 
     model, params = init_train_model(config, dataset.nid)
@@ -115,7 +124,6 @@ def main():
                 optimizer.step()
                 optimizer.zero_grad()
 
-            print(loss)
             if step % config.log_step == 0 and step != 0:
                 logger = f'epoch {epoch}/{config.epochs} ' \
                          f'step {step}/{len(dataloader)} ' \
@@ -130,14 +138,12 @@ def main():
         logger = f'{epoch} epoch time {(time.time() - epoch_start) // 60} minutes'
         print(logger)
 
-        if epoch % config.save_every == 0 or epoch == config.epochs:
-            checkpoint_name = str(Path(config.logs_dir, f'JDE-{epoch}.pt'))
-            checkpoint = {
-                'model': model.module.state_dict(),
-                'optimizer': optimizer.state_dict(),
-            }
+        checkpoint_name = str(Path(config.logs_dir, f'JDE-{epoch}.pt'))
+        checkpoint = {
+            'model': model.state_dict(),
+        }
 
-            torch.save(checkpoint, checkpoint_name)
+        torch.save(checkpoint, checkpoint_name)
 
         scheduler.step()
 